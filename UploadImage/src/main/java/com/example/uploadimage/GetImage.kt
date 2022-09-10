4a16a518951c3c0029b0d8e81a8c80ece8ed2d15
aniket <andypat257@gmail.com>
2022-09-10 21:46:44 +0530

    callback left

--- UploadImage/src/main/java/com/example/uploadimage/GetImage.kt
+++ UploadImage/src/main/java/com/example/uploadimage/GetImage.kt
@@ -1,32 +1,38 @@
 package com.example.uploadimage
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 import androidx.core.net.toUri
+import androidx.databinding.DataBindingUtil
 import androidx.loader.content.CursorLoader
 import com.example.uploadimage.databinding.ActivityGetImageBinding
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
+import com.example.uploadimage.databinding.AlertDialogLayoutBinding
+import java.io.*
 
 class GetImage : AppCompatActivity() {
 
     private lateinit var binding: ActivityGetImageBinding
     var intent1 = Intent()
-    lateinit var finalFile : File
+    lateinit var finalFile: File
     private val result: ActivityResultLauncher<Intent> =
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult(),
@@ -34,52 +40,44 @@ class GetImage : AppCompatActivity() {
                 if (it.resultCode == RESULT_OK) {
                     if (it.data!!.data == null) {
                         val photo = it.data?.extras?.get("data") as Bitmap
-                        binding.preview.setImageBitmap(photo)
                         binding.preview.visibility = View.VISIBLE
                         showImage(photo)
                     } else {
                         val uri = it.data?.data as Uri
-                        binding.preview.setImageURI(uri)
                         binding.preview.visibility = View.VISIBLE
-//                        val path = Environment.getExternalStorageDirectory().toString()
                         val path = this.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
-                        try{
+                        try {
                             val file = File(path, "/Image")
                             file.mkdirs()
                             val image = File(file.absolutePath, "image1.jpeg")
                             intent1.data = image.absolutePath.toUri()
-                            val source = FileInputStream(getRealPathFromURI(uri)?.let { it1 ->
-                                File(
-                                    it1
-                                )
-                            }).channel
-                            val destination = FileOutputStream(image).channel
-                            if (destination != null && source != null) {
-                                destination.transferFrom(source, 0, 2000000);
+                            val bitmap = BitmapFactory.decodeStream(this.contentResolver.openInputStream(uri))
+                            val out =  FileOutputStream(image)
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 70,out)
+                            println(image.length())
+                            if(image.length()>2000000){
+                               val bitmap = BitmapFactory.decodeFile(image.absolutePath)
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 70,out)
                             }
-                            source?.close()
-                            destination?.close()
-                        }catch (e : Exception){
-                            println(e.message)
+                            out.flush()
+                            out.close()
+                            binding.preview.setImageURI(image.toUri())
+                            println(image.absolutePath)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
                         }
 
                     }
 
                 } else
-                    Toast.makeText(this, "No", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Invalid data", Toast.LENGTH_LONG).show()
             })
 
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_get_image)
-//
-//        Toast.makeText(this,"Hay",Toast.LENGTH_LONG).show()
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         binding = ActivityGetImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -91,10 +89,10 @@ class GetImage : AppCompatActivity() {
         binding.done.setOnClickListener {
 
             val bundle = Bundle().also {
-                it.putString("path","hat")
+                it.putString("path", "hat")
             }
-            intent1.putExtra("path",bundle)
-            setResult(RESULT_OK,intent1)
+            intent1.putExtra("path", bundle)
+            setResult(RESULT_OK, intent1)
             finish()
 
         }
@@ -109,6 +107,7 @@ class GetImage : AppCompatActivity() {
         try {
             bitmap.compress(Bitmap.CompressFormat.JPEG, 70, fileOutputStream)
             intent1.data = image.absolutePath.toUri()
+            binding.preview.setImageBitmap(bitmap)
             fileOutputStream.flush()
             fileOutputStream.close()
         } catch (e: FileNotFoundException) {
@@ -121,29 +120,30 @@ class GetImage : AppCompatActivity() {
 
     private fun uploadImage() {
 
-        if (binding.camera.isChecked) {
-            result.launch(Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE_SECURE))
-        } else if (binding.gallery.isChecked) {
-            result.launch(Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI))
-        } else if (binding.file.isChecked) {
-            result.launch(
-                Intent(Intent.ACTION_GET_CONTENT).setType("image/jpg")
-                    .putExtra(Intent.EXTRA_LOCAL_ONLY, true)
-            )
-        } else {
+        val dialog = AlertDialog.Builder(this).create()
+        val dialogView = AlertDialogLayoutBinding.inflate(layoutInflater)
+        dialog.setView(dialogView.root)
+        dialog.show()
+
+        dialogView.camera.setOnClickListener {
+            binding.preview.setImageDrawable(getDrawable(R.drawable.ic_image_svgrepo_com))
+            dialog.dismiss()
             result.launch(Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE_SECURE))
         }
 
-    }
+        dialogView.gallery.setOnClickListener {
+            binding.preview.setImageDrawable(getDrawable(R.drawable.ic_image_svgrepo_com))
+            dialog.dismiss()
+            result.launch(Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI))
+        }
+        dialogView.files.setOnClickListener {
+            binding.preview.setImageDrawable(getDrawable(R.drawable.ic_image_svgrepo_com))
+            dialog.dismiss()
+            result.launch(
+                Intent(Intent.ACTION_GET_CONTENT).setType("image/*")
+                    .putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+            )
+        }
 
-    private fun getRealPathFromURI(contentUri: Uri): String? {
-        val proj = arrayOf(MediaStore.Images.Media.DATA)
-        val loader = CursorLoader(this, contentUri, proj, null, null, null)
-        val cursor = loader.loadInBackground()
-        val column_index = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        cursor?.moveToFirst()
-        val result = column_index?.let { cursor.getString(it) }
-        cursor?.close()
-        return result
     }
 }
